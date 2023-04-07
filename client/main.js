57ed317aa325e9ace55a949f0e809d0fbf6ae1dd
Wasix <66091116+WasixXD@users.noreply.github.com>
2023-04-07 18:34:58 -0300

    🧗 finded my way

---
+++ client/main.js
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+import { program } from "commander"
+import terminalLink from "terminal-link"
+const url = "http://localhost:3300/"
+
+
+program
+  .command("login")
+  .description("Auth your account")
+
+  .action(async (str, options) => {
+    let { name, pass } = await inquirer.prompt([{
+      type: "input",
+      message: "Enter your name:",
+      name: "name",
+
+    }, {
+      type: "password",
+      message: "Enter your password:",
+      mask: "*",
+      name: "pass"
+      }])
+
+
+    const request = await fetch(url + `user?name=${name}&pass=${pass}`)
+    const response = await request.json()
+  
+    if(terminalLink.isSupported) {
+      const link = terminalLink("Now open this link", url)
+    } else {
+      console.log("Now, open this link on your browser:", url +"auth/"+ response)
+      const inter = setInterval(async () => {
+        const auth = await fetch(`${url}authed/${response}`)
+        const { verify } = await auth.json()
+        
+        if(verify) {
+          clearInterval(inter)
+          console.log("Account authed")
+        }
+      },1000)  
+    }
+
+  
+  })
+
+program.parse()
